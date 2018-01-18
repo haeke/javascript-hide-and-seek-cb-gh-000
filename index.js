ce2d28e2b9e0ccc1d31b329ab660df1bf1d4de17
edwin rivera <waehake@gmail.com>
2018-01-18 16:40:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
   return document.getElementById('nested');
 }
 
-function increateRankBy(n) {
+function increaseRankBy(n) {
   var lis = document.querySelectorAll('ul.ranked-list');
   for (var i = 0; i < lis.length; i++) {
     lis[i].innerHTML = (i + 1).toString();
