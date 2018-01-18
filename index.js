764f9fa0989052ccc59b970b621958a4ccb34a8b
edwin rivera <waehake@gmail.com>
2018-01-18 17:10:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ function deepestChild() {
   var divs = document.querySelectorAll('#grand-node');
   
   var items = divs[0].querySelectorAll('div');
-  return items[items.length];
+  return items[items.length - 1];
 }
